5a8659d23567fbbf75505f9777374bc81fdd5f21
strelov1 <strelov1@gmail.com>
2018-09-29 14:17:57 +0300

    add test

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
 
-const Router = require('./router');
-const Middleware = require('./middleware');
-const download = require('./download');
+const Router = require("./router");
+const Middleware = require("./middleware");
+const download = require("./download");
 
 /**
  * Main Page
@@ -11,7 +11,7 @@ const download = require('./download');
  * @param {Response} response
  */
 function mainHandler(request, response) {
-	response.send('Main Page');
+    response.send("Main Page");
 }
 /**
  * File Download Page
@@ -19,26 +19,26 @@ function mainHandler(request, response) {
  * @param {Response} response
  */
 function getFileHandler(request, response) {
-	const downloadUrl = url.parse(request.url, true).query.url;
-	if (downloadUrl && typeof downloadUrl === 'string') {
-		download(downloadUrl, request.headers, response);
-	} else {
-		response.error('Not url in query params');
-	}
+    const downloadUrl = url.parse(request.url, true).query.url;
+    if (downloadUrl && typeof downloadUrl === "string") {
+        download(downloadUrl, request.headers, response);
+    } else {
+        response.error("Not url in query params");
+    }
 }
 
 const server = http.createServer((request, response) => {
-	// Add Response Helpers
-	Middleware.responseWraper(response);
+    // Add Response Helpers
+    Middleware.responseWraper(response);
 
-	const router = new Router(request, response);
+    const router = new Router(request, response);
 
-	router.get('/', mainHandler);
-	router.get('/getfile', getFileHandler);
+    router.get("/", mainHandler);
+    router.get("/getfile", getFileHandler);
 
-	router.run();
+    router.run();
 });
 
 server.listen(8000, () => {
-	console.log('Run server http://localhost:8000');
+    console.log("Run server http://localhost:8000");
 });

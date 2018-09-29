842d85e80a255d3b68a50d5bcb32aa50e1e001ff
strelov1 <strelov1@gmail.com>
2018-09-29 14:38:04 +0300

    improve test

--- __test__/download.test.js
+++ __test__/download.test.js
@@ -6,38 +6,50 @@ const downloadFile = `${__dirname}/downloadFile.tmp`;
 const downloadMockFile = `${__dirname}/download.mock.file.txt`;
 const existMockFile = `${__dirname}/mock.file.txt`;
 
-describe('Client headers', () => {
-	const fleStream = fs.createWriteStream(downloadFile);
+const reponseStreamMock = (filename) => {
+	const fleStream = fs.createWriteStream(filename);
 	fleStream.headers = {};
 	fleStream.setHeaders = (headers) => {
 		fleStream.headers = headers;
 	};
-	download('http://nodejs.org/dist/index.json', {}, fleStream).then(() => {
-		// Server Headers
-		expect(fleStream.headers.server).toBe('cloudflare');
-		// Downlaod client Headers
-		expect(fleStream.headers['Content-disposition']).toBe(`attachment; filename='index.json'`);
-		fs.unlinkSync(downloadFile);
-	});
+	fleStream.error = (e) => e;
+	return fleStream;
+};
+
+describe('Client headers', () => {
+	const responseStream = reponseStreamMock(downloadFile);
+	download('http://nodejs.org/dist/index.json', {}, responseStream)
+		.then(() => {
+			// Server Headers
+			expect(responseStream.headers.server).toBe('cloudflare');
+			// Downlaod client Headers
+			expect(responseStream.headers['Content-disposition']).toBe(`attachment; filename='index.json'`);
+			fs.unlinkSync(downloadFile);
+		})
+		.catch((e) => {
+			expect(e).toBe(undefined);
+			fs.unlinkSync(downloadFile);
+		});
 });
 
 test('Client download', async () => {
-	const fleStreamDownlaod = fs.createWriteStream(downloadMockFile);
-	fleStreamDownlaod.headers = {};
-	fleStreamDownlaod.setHeaders = (headers) => {
-		fleStreamDownlaod.headers = headers;
-	};
+	const responseStream = reponseStreamMock(downloadFile);
 
 	download(
 		'https://raw.githubusercontent.com/strelov1/download-server/master/__test__/mock.file.txt',
 		{},
-		fleStreamDownlaod
-	).then(() => {
-		fs.readFile(existMockFile, 'utf8', (errMockFile, contentsMockFile) => {
-			fs.readFile(downloadMockFile, 'utf8', (errDownloadFile, contentsDownloadFile) => {
-				expect(contentsMockFile).toEqual(contentsDownloadFile);
-				fs.unlinkSync(downloadMockFile);
+		responseStream
+	)
+		.then(() => {
+			fs.readFile(existMockFile, 'utf8', (errMockFile, contentsMockFile) => {
+				fs.readFile(downloadMockFile, 'utf8', (errDownloadFile, contentsDownloadFile) => {
+					expect(contentsMockFile).toEqual(contentsDownloadFile);
+					fs.unlinkSync(downloadMockFile);
+				});
 			});
+		})
+		.catch((e) => {
+			expect(e).toBe(undefined);
+			fs.unlinkSync(downloadMockFile);
 		});
-	});
 });
